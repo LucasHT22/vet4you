6aebb8ae97cfbbdb3b8a075859cf1e1e05b3d8de
Less <duduless2020@gmail.com>
2025-10-21 17:24:41 -0300

    Add files via upload

---
+++ view/App.java
@@ -0,0 +1,25 @@
+package com.mycompany.view;
+
+
+import com.mycompany.view.ADmin;
+import javax.swing.SwingUtilities;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ADmin().setVisible(true);
+    }
+    
+}
